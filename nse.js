6d77a319d078d94b4e5e183e3def1bee0f721ade
best800 <119391599+best800@users.noreply.github.com>
2023-01-08 16:49:14 +0530

    Updatejs

--- nse.js
+++ nse.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
 function UpdateData() {
     var headers = {
         "accept-language": "en-US,en;q=0.9",
-        "Access-Control-Allow-Origin": "nseindia.com",
         "cookie":"665B05C7A5E8DDCFB2D9E7BB4C523503~YAAQJSEPF4xSChqFAQAAd1pGhRJ0Ne/efBR4goJkR7/cRycX51gQM4s+kQzS4wVd3BA+I6PTdSIvs16rkn71NAiEMuN8f7NRmbiQDop+vCVTEyxQ81qmrwLLUIr+vttju+99mipjJIs08iUwxYeDfTSHwH3NfENZ7nyNtgpG+euwkO1rUBlwGLSfbgizI8S92y3CSiJ6jv/46LOcud3B9LQNsB/zgNRov4DGkU4wl9gcf0xjYbPE0BfF8lFMVJgaHfTe~1",
         "scheme": "https",
         "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
@@ -13,7 +12,7 @@ function UpdateData() {
     }
     $.ajax({
         url: Url,
-        type: "POST",
+        type: "GET",
         dataType: 'json',
         headers: headers,
         success: function (data) {
