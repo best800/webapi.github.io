0a6942dd540743b11f250cd2b5a49dc8096aacba
best800 <119391599+best800@users.noreply.github.com>
2023-01-08 16:50:51 +0530

    update js

--- nse.js
+++ nse.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
 function UpdateData() {
     var headers = {
         "accept-language": "en-US,en;q=0.9",
-        "cookie":"665B05C7A5E8DDCFB2D9E7BB4C523503~YAAQJSEPF4xSChqFAQAAd1pGhRJ0Ne/efBR4goJkR7/cRycX51gQM4s+kQzS4wVd3BA+I6PTdSIvs16rkn71NAiEMuN8f7NRmbiQDop+vCVTEyxQ81qmrwLLUIr+vttju+99mipjJIs08iUwxYeDfTSHwH3NfENZ7nyNtgpG+euwkO1rUBlwGLSfbgizI8S92y3CSiJ6jv/46LOcud3B9LQNsB/zgNRov4DGkU4wl9gcf0xjYbPE0BfF8lFMVJgaHfTe~1",
         "scheme": "https",
+		Access-Control-Allow-Origin:'*',
         "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
         "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     }
