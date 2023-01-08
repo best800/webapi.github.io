b785a30f356b30ebbea38f59a88e189b55778aac
best800 <119391599+best800@users.noreply.github.com>
2023-01-08 19:53:52 +0530

    update

--- nse.js
+++ nse.js
@@ -3,12 +3,7 @@ $(document).ready(function () {
     UpdateData();
 })
 function UpdateData() {
-    var headers = {
-        "accept-language": "en-US,en;q=0.9",
-		"Access-Control-Allow-Origin":"*",
-        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization",
-        "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    }
+    
     $.ajax({
         url: Url,
         type: "GET",
